7482b133519c6d1d765faf226c0b4072b98bc3b6
pauloh.pires <pauloh.pires@inss.gov.br>
2023-11-16 04:57:16 +0000

    feat: trocando um authorization code pelos tokens

--- src/authorization-code-flow/index.ts
+++ src/authorization-code-flow/index.ts
@@ -13,6 +13,25 @@ app.get('/login', (req, res) => {
     res.redirect(url);
 });
 
+app.get('/callback', async (req, res) => {
+    const bodyParams = new URLSearchParams({
+        client_id: 'fullcycle-client',
+        grant_type: 'authorization_code',
+        code: req.query.code as string,
+        redirect_uri: 'http://localhost:3000/callback',
+    })
+    const url = `http://keycloak:8080/realms/fullcycle-realm/protocol/openid-connect/token`;
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: bodyParams.toString(),
+    });
+    const result = await response.json();
+    res.json(result);
+});
+
 app.listen(3000, () => {
     console.log('listening on port 3000');
 });
