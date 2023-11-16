e0efc1782c9314ae079ac05e8a529847ff07d1e5
pauloh.pires <pauloh.pires@inss.gov.br>
2023-11-16 04:15:14 +0000

    feat: redirecionamento apra o Keycloak

--- src/authorization-code-flow/index.ts
+++ src/authorization-code-flow/index.ts
@@ -2,8 +2,15 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hi there!');
+app.get('/login', (req, res) => {
+    const loginParams = new URLSearchParams({
+        client_id: 'fullcycle-client',
+        redirect_uri: 'http://localhost:3000/callback',
+        response_type: 'code',
+        scope: 'openid',
+    })
+    const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginParams}`;
+    res.redirect(url);
 });
 
 app.listen(3000, () => {
